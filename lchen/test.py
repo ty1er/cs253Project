d77c07140cc711f78dd1d99e6ee9e9d19287ce99
suifengls <suifengls@gmail.com>
2013-11-26 12:53:00 -0800

    a test for mongodb

--- lchen/test.py
+++ lchen/test.py
@@ -1,4 +1,11 @@
-import rethinkdb as r
-r.connect('localhost', 28015).repl()
-r.db('test').table_create('tv_shows').run()
-r.table('tv_shows').insert({'name':'Star Trek TNG' }).run()
+from pymongo import MongoClient
+import datetime
+client = MongoClient('n1', 27017)
+db = client.test
+print db.name
+print db.my_collection
+
+# set up replica set
+client.admin.command("replSetInitiate")
+
+
